68c87c9f9e28ee8d08462759f0248249936ec3cc
Dmitria <Dmitria1994@gmail.com>
2019-02-02 17:49:57 +0700

    Add class for cli argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <vector>
+#include "arg_parser.h"
 
-int main() {
-	std::cout << "This is skeleton for Ymir" << std::endl;
+int main(int argc, char** argv) {
+    Ymir::utils::ArgParser argParser(argc, argv);
+    argParser
+        .addParam("--file", "Path to file with source code", true)
+        .addParam("--help", "Print help");
+    try {
+        argParser.parse();
+        if (argParser.has("--help")) {
+            const auto header = "Overview : Ymir C compiler";
+            argParser.help(header, std::cout);
+            return 0;
+        }
+        if (!argParser.has("--file")) {
+            std::cout << "--file option is required\n";
+            return 0;
+        }
+        std::cout << argParser.get("--file") << std::endl;
+        return 0;
+    }
+    catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        argParser.help("You can use:", std::cout);
+        return 1;
+    }
 }
---
+++ src/utils/arg_parser.cpp
@@ -0,0 +1,99 @@
+#include "arg_parser.h"
+
+namespace Ymir::utils
+{
+
+namespace
+{
+
+enum class TypeArg
+{
+    Key,
+    Value,
+    Error
+};
+
+TypeArg classify(const std::string_view param) {
+    switch (param.size()) {
+        case 0:
+            return TypeArg::Error;
+        case 1:
+        case 2:
+            return param.find('-') == std::string::npos ? TypeArg::Value : TypeArg::Error;
+        default:
+            return param.substr(0, 2) == "--" ?  TypeArg::Key : TypeArg::Value;
+    }
+}
+
+} // namespace
+
+ArgParser::ArgParser(const int argc, char** argv)
+    : argc_(argc)
+    , argv_(argv)
+{
+    if (argv == nullptr) {
+        throw std::runtime_error("Invalid argv list");
+    }
+    if (argc < 1) {
+        throw std::runtime_error("Unexpected args count = " + std::to_string(argc));
+    }
+}
+
+ArgParser& ArgParser::addParam(const std::string& name,
+                               const std::string& desc,
+                               const bool has_value) {
+    paramToAttr_[name] = {desc, has_value};
+    return *this;
+}
+
+void ArgParser::help(const std::string_view helpHeader, std::ostream& stream) const {
+    stream << helpHeader << "\n";
+    stream << "OPTIONS:" << "\n";
+    for (const auto& [name, attr] : paramToAttr_) {
+        stream << "  " << name << " - " << attr.desc << "\n";
+    }
+}
+
+bool ArgParser::has(const std::string_view name) const {
+    return paramToVal_.find(name) != paramToVal_.end();
+}
+
+std::string_view ArgParser::get(const std::string_view name) const {
+    return paramToVal_.find(name)->second;
+}
+
+void ArgParser::parse() {
+    using namespace std::string_literals;
+    auto nextArg =
+        [this, current{argv_ + 1}](std::string_view& arg) mutable -> bool { // First arg is programm name
+            if (current == argv_ + argc_) {
+                return false;
+            }
+            arg = *current++;
+            return true;
+        };
+
+    std::string_view arg;
+    while (nextArg(arg)) {
+        switch (classify(arg)) {
+            case TypeArg::Key: {
+                const auto found = paramToAttr_.find(arg);
+                if (found != paramToAttr_.end()) {
+                    std::string_view val;
+                    if (found->second.has_value) {
+                        if (!nextArg(val) || classify(val) != TypeArg::Value) {
+                            throw std::runtime_error("Expected val for parameter "s + arg.data());
+                        }
+                    }
+                    paramToVal_.emplace(found->first, val);
+                    break;
+                }
+            }
+            default: {
+                throw std::runtime_error("Unexpected parameter "s + arg.data());
+            }
+        }
+    }
+}
+
+} // namespace Ymir
